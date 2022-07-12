2f488bc617fd8cfab47552f5e484da1a7e9fbc48
dsteyn.software@gmail.com <dsteyn.software@gmail.com>
2022-07-12 12:28:09 +0200

    Add project files.

---
+++ ElectricityTrackerLibrary/Models/AddressModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricityTrackerLibrary.Models
+{
+    public class AddressModel
+    {
+        public int Id { get; set; }
+        public string FlatUnit { get; set; }
+        public string Street { get; set; }
+        public string Suburb { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
